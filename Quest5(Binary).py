a09da35f2889cb5955cfce0273fd06f2c5e5d555
Hemraj0808 <149263495+Hemraj0808@users.noreply.github.com>
2024-07-23 20:46:08 +0530

    Add files via upload

---
+++ Quest5(Binary).py
@@ -0,0 +1,30 @@
+'''Python program to convert decimal to hexadecimal
+After 9 reminder we write 10 = A, 11 = B, 12 = C, and so on.....
+Bitwise Operator:
+
+and (&)
+or(|)
+xor(^) - like subtraction (1,1=0; 0,0=0)
+left shift (<<) -
+right shift (>>)'''
+
+
+# Bitwise operations
+
+# 1. and
+print(10&12)
+print("-----------------------------------------")
+
+# 2. or
+print(10|12)
+print("-----------------------------------------")
+
+# 3. xor
+print(10^12)
+print("-----------------------------------------")
+
+# 4. Shift left
+print(10<<2)
+
+# 5. Right Shift
+print(10>>2)
